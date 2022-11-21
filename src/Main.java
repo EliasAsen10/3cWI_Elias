5543815ff61d8e56605554d783212b9890c4852c
elias <you@example.com>
2022-11-21 12:28:39 +0100

    firstExerciseOO

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-import java.util.Random;
+import at.ran.objects.Car;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
+ public static void main(String[] args) {
+
+
+  Car c1 = new Car();
+  c1.brand = "Audi";
+  c1.fuelConsumption= 7;
+  c1.serialNumber= "A1234";
+
+
+  Car c2 = new Car();
+  c2.brand = "Mercedes";
+  c2.fuelConsumption = 7;
+  c2.serialNumber = "A1234n";
+  System.out.println(c1.serialNumber);
+  System.out.println(c2.serialNumber);
+
+
+ }
 }
+
---
+++ src/at/ran/objects/Car.java
@@ -0,0 +1,8 @@
+package at.ran.objects;
+
+public class Car {
+public int fuelConsumption;
+public String brand;
+public String serialNumber;
+private String color;
+}
