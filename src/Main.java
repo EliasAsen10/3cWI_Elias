51c2385b0a892e6904734bd372b23607cadf6200
elias <you@example.com>
2022-11-24 09:10:48 +0100

    03-Constructor&Caps

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,38 @@ import at.ran.objects.Car;
 
 public class Main {
  public static void main(String[] args) {
-
+/* do that later
 
   Car c1 = new Car();
   c1.brand = "Audi";
   c1.fuelConsumption= 8;
   c1.serialNumber= "A1234";
+  c1.fuelAmount= 70;
 
 
   Car c2 = new Car();
   c2.brand = "Mercedes";
-  c2.fuelConsumption = 7;
+  c2.fuelConsumption = 6;
   c2.serialNumber = "A1234n";
+  c2.fuelAmount=20;
+*/
+  Car c3  = new Car( 17,"opel", "1234" );
+
+
+
+  System.out.println(c3.serialNumber);
+
+
+
+/*
   System.out.println(c1.serialNumber);
   System.out.println(c2.serialNumber);
 
+  System.out.println(c2.fuelAmount);
+  c2.drive();
+  System.out.println(c2.fuelAmount);
+
+*/
 
  }
 }
--- src/at/ran/objects/Car.java
+++ src/at/ran/objects/Car.java
@@ -5,4 +5,15 @@ public int fuelConsumption;
 public String brand;
 public String serialNumber;
 private String color;
+
+public Car(int fuelConsumption, String brand, String serialNumber){
+this.fuelConsumption = fuelConsumption;
+this.serialNumber = serialNumber;
 }
+
+public int fuelAmount;
+
+public void drive() {
+    this.fuelAmount = this.fuelAmount - fuelConsumption;
+    System.out.println("I am drive");
+}}
