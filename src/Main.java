a535ba565412c5a87b8b9b61ce3804d8ae26c059
elias <you@example.com>
2022-12-01 08:59:43 +0100

    Gettersetter&Public/Private

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,24 @@ import at.ran.objects.Car;
 
 public class Main {
  public static void main(String[] args) {
-/* do that later
+
 
   Car c1 = new Car();
-  c1.brand = "Audi";
-  c1.fuelConsumption= 8;
-  c1.serialNumber= "A1234";
-  c1.fuelAmount= 70;
+  c1.setBrand("Audi");
+  c1.setFuelConsumption("20");
+  c1.setSerialNumber("A1234");
+  c1.setFuelAmount(70);
+  c1.setColor("green");
 
-
+  System.out.println(c1.getBrand());
+/*
   Car c2 = new Car();
   c2.brand = "Mercedes";
   c2.fuelConsumption = 6;
   c2.serialNumber = "A1234n";
   c2.fuelAmount=20;
-*/
+
+
   Car c3  = new Car( 17,"opel", "1234" );
 
 
@@ -25,7 +28,7 @@ public class Main {
 
 
 
-/*
+
   System.out.println(c1.serialNumber);
   System.out.println(c2.serialNumber);
 
@@ -34,7 +37,5 @@ public class Main {
   System.out.println(c2.fuelAmount);
 
 */
-
  }
 }
-
--- src/at/ran/objects/Car.java
+++ src/at/ran/objects/Car.java
@@ -1,19 +1,77 @@
 package at.ran.objects;
 
 public class Car {
-public int fuelConsumption;
-public String brand;
-public String serialNumber;
-private String color;
+    private String fuelConsumption;
+    private String brand;
+    private String serialNumber;
+    private String color;
+    private int fuelAmount;
 
+
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setFuelConsumption(String fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+}
+
+
+
+    /*
 public Car(int fuelConsumption, String brand, String serialNumber){
 this.fuelConsumption = fuelConsumption;
 this.serialNumber = serialNumber;
 }
+*/
 
-public int fuelAmount;
+/*
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am drive");
+    }
 
-public void drive() {
-    this.fuelAmount = this.fuelAmount - fuelConsumption;
-    System.out.println("I am drive");
-}}
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+}
+
+*/
